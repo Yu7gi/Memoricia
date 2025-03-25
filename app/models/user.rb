b277af837b50894272a1fe647331be7133aff797
Yu7gi <piano.yuna26@gmail.com>
2025-03-25 09:34:48 +0000

    ログイン機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:name]
+
+  validates :name, length: {minimum:2,maximum:15}, uniqueness: true
+end
