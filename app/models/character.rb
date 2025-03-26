67385a620475678542de79136cba658c492ac1fe
Yu7gi <piano.yuna26@gmail.com>
2025-03-26 18:01:15 +0000

    キャラクターnew, show, edit画面作成

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,10 @@
 class Character < ApplicationRecord
-  has_one_attached :image
+  has_one_attached :standing_picture
+  has_one_attached :expression_1
+  has_one_attached :expression_2
+  has_one_attached :expression_3
+  has_one_attached :expression_4
+  has_one_attached :background_image
 
   scope :publicly_visible, -> { where(public: true) }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'characters/index'
-  get 'characters/show'
-  get 'characters/edit'
-  get 'characters/new'
-
+  
   scope module: :public do
     resources :characters, only: [:new, :index, :show, :create, :edit, :update, :destroy]
   end
